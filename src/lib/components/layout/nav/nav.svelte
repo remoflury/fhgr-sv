ad3437467fce0134158967deac34beda93304e57
remoflury <remoflury97+web@gmail.com>
2025-02-02 13:44:25 +0100

    nav bar

---
+++ src/lib/components/layout/nav/nav.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Menubar from '$lib/components/ui/menubar';
+	import { CodeXml } from 'lucide-svelte';
+	import LightSwitch from './lightSwitch.svelte';
+
+	const items: {
+		label: string;
+		href: string;
+	}[] = [
+		{
+			href: '/quicklinks',
+			label: 'Quicklinks'
+		},
+		{
+			href: '/quests',
+			label: 'Aufgaben'
+		}
+	];
+</script>
+
+<nav class="container my-2">
+	<Menubar.Root class="items-center justify-end py-1 pl-4">
+		<a href="/" class="col-span-1 mr-auto max-w-max" title="zur Startseite">
+			<CodeXml class="stroke-primary" />
+		</a>
+		{#each items as item}
+			<Menubar.Menu>
+				<Button variant="link" href={item.href}>{item.label}</Button>
+			</Menubar.Menu>
+		{/each}
+		<Menubar.Menu>
+			<LightSwitch />
+		</Menubar.Menu>
+	</Menubar.Root>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
+	import Nav from '$lib/components/layout/nav/nav.svelte';
+	import { ModeWatcher } from 'mode-watcher';
 	import '../css/main.css';
 	let { children } = $props();
 </script>
+
+<ModeWatcher />
+<Nav />
 
 <main class="container">
 	{@render children()}
