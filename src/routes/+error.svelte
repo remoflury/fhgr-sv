339f633302982450246910e98a438a883e6b5c60
remoflury <remoflury97+web@gmail.com>
2025-03-04 09:34:59 +0100

    add error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
+<section class="section-spacing container">
+	<h1>Ups, da lief etwas schief.</h1>
+
+	<p>Das ist eine +error.svelte Seite, welche von SvelteKit bei einem Error geladen wird.</p>
+
+	<Button href="/" class="mt-8">Zur Startseite</Button>
+</section>
+
+<svelte:head>
+	<title>Error</title>
+</svelte:head>
