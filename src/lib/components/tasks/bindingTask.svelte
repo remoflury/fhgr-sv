83d08b960dc26453cfec782c867987410262d951
remoflury <remoflury97+web@gmail.com>
2025-04-04 12:36:02 +0200

    feat(bindingTask): replaced binding value with tween

--- src/lib/components/tasks/bindingTask.svelte
+++ src/lib/components/tasks/bindingTask.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-	let value = $state(20);
+	import { sineInOut } from 'svelte/easing';
+	import { Tween } from 'svelte/motion';
+
+	const tween = new Tween(20, { duration: 125, easing: sineInOut });
 </script>
 
 <div class="flex flex-wrap items-center gap-x-4 gap-y-2 lg:gap-x-8">
@@ -10,13 +13,14 @@
 		class="max-w-80 accent-primary focus:outline-none"
 		min="0"
 		max="100"
-		bind:value
+		bind:value={tween.target}
 	/>
 </div>
 
 <div class="relative mt-4 h-6 w-60 max-w-full border border-primary" role="progressbar">
-	<span class="absolute inset-0 block bg-primary transition" style="width: {value}%"> </span>
+	<span class="absolute inset-0 block bg-primary transition" style="width: {tween.current}%">
+	</span>
 	<small class="absolute -right-4 top-1/2 -translate-y-1/2 translate-x-full"
-		>{value.toFixed(2)}%</small
+		>{tween.current.toFixed(2)}%</small
 	>
 </div>
